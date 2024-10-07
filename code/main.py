a023eeab7da9fc039243c2f664340c7efa534eb0
Ethan Kuo <ek0335@berkeley.edu>
2024-10-07 11:55:15 -0700

    done with part 3 (morph sequence

--- code/main.py
+++ code/main.py
@@ -2,16 +2,13 @@ import json
 from scipy.spatial import Delaunay
 import skimage.io as skio
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage.draw import polygon
-import cv2
 
-from utils import display_img_with_keypoints, pick_and_record_keypoints, compute_affine, homogeneous_coords, bilinear_interpolation, display_img
+from utils import display_img_with_keypoints, display_img, pick_and_record_keypoints, compute_intermediate, morph
 
 # ===== Part 0: Parameters =====
-PICK_KEYPOINTS = True
-img1_name = 'ethan'
-img2_name = 'shai'
+PICK_KEYPOINTS = False
+img1_name = 'tzuyu'
+img2_name = 'minjoo'
 
 # ===== Part 1: Defining Correspondences =====
 # Define and display the shapes for each image
@@ -29,72 +26,17 @@ else:
         data = json.load(f)
     img1_keypoints, img2_keypoints = np.array(data['image1_keypoints']), np.array(data['image2_keypoints'])
 
-display_img_with_keypoints(img1, img1_keypoints)
-display_img_with_keypoints(img2, img2_keypoints)
+# display_img_with_keypoints(img1, img1_keypoints)
+# display_img_with_keypoints(img2, img2_keypoints)
 
 # ===== Part 2: Computing the "Mid-way Face" =====
-# Compute and display triangulation of average shape
+# Compute triangulation of average shape -- this will be used throughout the morphing sequence
 avg_keypoints = (img1_keypoints + img2_keypoints) / 2
 triangulation = Delaunay(avg_keypoints)
-plt.figure()
-plt.triplot(avg_keypoints[:, 1], avg_keypoints[:, 0], triangulation.simplices, color='blue')
-plt.scatter(avg_keypoints[:, 1], avg_keypoints[:, 0], color='red')
-plt.axis('equal')
-plt.gca().invert_yaxis()
-plt.show()
 
-morphed_img1 = np.zeros_like(img1)
-morphed_img2 = np.zeros_like(img2)
-midway_img = np.zeros_like(img2)
-for i, j, k in triangulation.simplices:
-    # Compute affine transformation matrix for both images (from average to source image)
-    avg_tri = np.array((avg_keypoints[i], avg_keypoints[j], avg_keypoints[k]))
-    img1_tri = np.array((img1_keypoints[i], img1_keypoints[j], img1_keypoints[k]))
-    img2_tri = np.array((img2_keypoints[i], img2_keypoints[j], img2_keypoints[k]))
-    A = compute_affine(avg_tri, img1_tri)
-    B = compute_affine(avg_tri, img2_tri)
-    # print('Affine matrix for img1:')
-    # print(A)
-    # print('Affine matrix for img2:')
-    # print(B)
+midway_img = compute_intermediate(img1, img2, img1_keypoints, img2_keypoints, triangulation, 1/2, 1/2)
+# display_img(midway_img)
+# skio.imsave(f'../images/{img1_name}_to_{img2_name}.jpg', midway_img)
 
-    # Get the coordinates inside the triangle in the average image
-    # mask = np.zeros((img1.shape[0], img1.shape[1]), dtype=np.uint8)
-    # cv2.fillConvexPoly(mask, np.array(avg_tri, dtype=np.int32), 1)
-    # avg_tri_coords = np.nonzero(mask)
-    # avg_tri_coords = np.array(list(zip(avg_tri_coords[0], avg_tri_coords[1])))
-    row_coords, col_coords = polygon(avg_tri[:, 0], avg_tri[:, 1])
-    avg_tri_coords = np.array(list(zip(row_coords, col_coords)))
-
-    # Apply the transformation matrices to the average triangle
-    avg_tri_coords_matrix = np.column_stack(homogeneous_coords(avg_tri_coords))
-    img1_preimage_coords_matrix = A @ avg_tri_coords_matrix
-    img2_preimage_coords_matrix = B @ avg_tri_coords_matrix
-
-    # print('avg_tri_coords_matrix.shape: ', avg_tri_coords_matrix.shape)
-    # print('img1_preimage_coords_matrix.shape: ', img1_preimage_coords_matrix.shape)
-    # print('img2_preimage_coords_matrix.shape: ', img2_preimage_coords_matrix.shape)
-
-    # Sample colors for each preimage using bilinear interpolation
-    img1_preimage_coords = img1_preimage_coords_matrix[:2].T # unpack the raw coordiantes from homogeneous matrix
-    img2_preimage_coords = img2_preimage_coords_matrix[:2].T
-    # print('img1_preimage_coords: ', img1_preimage_coords.shape)
-    # print('img2_preimage_coords: ', img2_preimage_coords.shape)
-    img1_preimage_colors = bilinear_interpolation(img1, img1_preimage_coords)
-    img2_preimage_colors = bilinear_interpolation(img2, img2_preimage_coords)
-    # print('img1_preimage_colors', img1_preimage_colors[0])
-
-    # Assign averaged colors to the average triangle
-    averaged_colors = (img1_preimage_colors + img2_preimage_colors) / 2
-    for idx, (r, c) in enumerate(avg_tri_coords):
-        morphed_img1[r,c] = img1_preimage_colors[idx]
-        morphed_img2[r,c] = img2_preimage_colors[idx]
-        midway_img[r,c] = averaged_colors[idx]
-
-# Display midway image
-display_img(morphed_img1)
-display_img(morphed_img2)
-display_img(midway_img)
-
-# Save midway image
-skio.imsave(f'../images/{img1_name}_to_{img2_name}.jpg', midway_img)
+# ===== Part 3: The Morph Sequence =====
+morph(img1_name, img2_name, img1, img2, img1_keypoints, img2_keypoints, triangulation)
--- code/utils.py
+++ code/utils.py
@@ -4,6 +4,10 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 import json
 import skimage.io as skio
+from scipy.spatial import Delaunay
+from skimage.draw import polygon
+from PIL import Image
+
 
 def pick_and_record_keypoints(img1, img2, json_path):
     # Create empty lists to store the points for each image
@@ -78,6 +82,15 @@ def display_img(img):
     plt.imshow(img)
     plt.show()
 
+def display_triangulation(keypoints, triangulation):
+    plt.figure()
+    plt.triplot(keypoints[:, 1], keypoints[:, 0], triangulation.simplices, color='blue')
+    plt.scatter(keypoints[:, 1], keypoints[:, 0], color='red')
+    plt.axis('equal')
+    plt.gca().invert_yaxis()
+    plt.show()
+
+
 # Compute an affine transformation matrix between 2 triangles
 def compute_affine(tri1, tri2):
     p1_x, p1_y = tri1[0][0], tri1[0][1]
@@ -129,3 +142,66 @@ def bilinear_interpolation(img, coords):
     interpolated_colors = np.stack(interpolated_colors, axis=-1)
 
     return interpolated_colors
+
+def compute_intermediate(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve_frac):
+    assert im1.shape == im2.shape
+    morphed_img1 = np.zeros_like(im1)
+    morphed_img2 = np.zeros_like(im1)
+    avg_img = np.zeros_like(im1)
+
+    # Compute the intermediate shape based on the warp fraction
+    avg_pts = im2_pts * warp_frac + im1_pts * (1-warp_frac)
+
+    for i, j, k in tri.simplices:
+        # Compute affine transformation matrix for both images (from intermediate to source image)
+        avg_tri = np.array((avg_pts[i], avg_pts[j], avg_pts[k]))
+        img1_tri = np.array((im1_pts[i], im1_pts[j], im1_pts[k]))
+        img2_tri = np.array((im2_pts[i], im2_pts[j], im2_pts[k]))
+        A = compute_affine(avg_tri, img1_tri)
+        B = compute_affine(avg_tri, img2_tri)
+
+        # Get the coordinates inside the triangle in the average image
+        row_coords, col_coords = polygon(avg_tri[:, 0], avg_tri[:, 1])
+        avg_tri_coords = np.array(list(zip(row_coords, col_coords)))
+
+        # Apply the transformation matrices to the average triangle
+        avg_tri_coords_matrix = np.column_stack(homogeneous_coords(avg_tri_coords))
+        img1_preimage_coords_matrix = A @ avg_tri_coords_matrix
+        img2_preimage_coords_matrix = B @ avg_tri_coords_matrix
+
+        # Sample colors for each preimage using bilinear interpolation
+        img1_preimage_coords = img1_preimage_coords_matrix[:2].T # unpack the raw coordiantes from homogeneous matrix
+        img2_preimage_coords = img2_preimage_coords_matrix[:2].T
+        img1_preimage_colors = bilinear_interpolation(im1, img1_preimage_coords)
+        img2_preimage_colors = bilinear_interpolation(im2, img2_preimage_coords)
+
+        # Assign averaged colors to the average triangle
+        averaged_colors = img2_preimage_colors * dissolve_frac + img1_preimage_colors * (1-dissolve_frac)
+        for idx, (r, c) in enumerate(avg_tri_coords):
+            morphed_img1[r,c] = img1_preimage_colors[idx]
+            morphed_img2[r,c] = img2_preimage_colors[idx]
+            avg_img[r,c] = averaged_colors[idx]
+    
+    return avg_img
+
+    # Display midway image
+    display_img(morphed_img1)
+    display_img(morphed_img2)
+    display_img(avg_img)
+
+    # Save midway image
+    skio.imsave(f'../images/{img1_name}_to_{img2_name}.jpg', midway_img)
+
+
+# Morph im1 to im2 using the triangulation 
+def morph(im1_name, im2_name, im1, im2, im1_pts, im2_pts, tri, warp_frac=1/45, dissolve_frac=1/45, frames=45):
+    morph_sequence = []
+    for t in range(frames+1):
+        morph_sequence.append(compute_intermediate(im1, im2, im1_pts, im2_pts, tri, warp_frac * t, dissolve_frac * t))
+
+    # Save a GIF of the morph sequence
+    save_gif(morph_sequence, f'../images/{im1_name}_to_{im2_name}.gif')
+
+def save_gif(morph_sequence, output_path, duration=100):
+    images = [Image.fromarray((image_array).astype(np.uint8)) for image_array in morph_sequence]
+    images[0].save(output_path, save_all=True, append_images=images[1:], duration=duration, loop=0)
